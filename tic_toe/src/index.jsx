63054355a9820babd0cfe676ce1f7d423719bb5e
Meiyi Zheng <meiyizheng@yahoo.com>
2024-06-19 23:08:39 -0700

    Add tic toe

---
+++ tic_toe/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Game } from './App.jsx'
+
+ReactDOM.createRoot( 
+  document.querySelector('#root')
+).render(<Game />)
