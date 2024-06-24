ab28e8b880e8a2158c246afe75357ec397821a1b
Meiyi Zheng <meiyizheng@yahoo.com>
2024-06-23 17:06:57 -0700

    Add product table

--- tic_toe/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import { Game } from './App.jsx'
-
-ReactDOM.createRoot( 
-  document.querySelector('#root')
-).render(<Game />)
